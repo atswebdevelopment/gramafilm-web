d474eecaba37454acda2c23ecd40036eca3c277a
Adam Southorn <adam@atswebdevelopment.com>
2020-07-16 01:32:06 +0100

    Not ready for SSL yet

--- apollo/config.js
+++ apollo/config.js
@@ -2,7 +2,7 @@ import cache from './cache'
 
 export default function (ctx) {
   return {
-    httpEndpoint: process.env.NODE_ENV === 'production' ? 'https://18.132.131.54/graphql' : 'http://localhost:1337/graphql',
+    httpEndpoint: process.env.NODE_ENV === 'production' ? 'http://18.132.131.54/graphql' : 'http://localhost:1337/graphql',
     cache
   }
 }
