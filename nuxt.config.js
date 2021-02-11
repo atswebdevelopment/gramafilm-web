0fa6db6a26ee3102664d8d5a5fc8cab0878e7632
Adam Southorn <adam@atswebdevelopment.com>
2021-02-11 20:02:33 +0000

    Revert "Go away PWA"
    This reverts commit 9d851da3892a0557b4e0af5d364c2c61d3ea6906.

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,7 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/dotenv',
     '@nuxtjs/eslint-module',
+    ['@nuxtjs/pwa'],
     '@nuxtjs/style-resources',
     ['@nuxtjs/google-analytics', {
       id: 'UA-22891163-1'
@@ -109,6 +110,92 @@ export default {
   apollo: {  
     clientConfigs: {
       default: '~/apollo/config.js'
+    }
+  },
+  pwa: {
+    manifest: {
+      name: 'gramafilm',
+      short_name: 'gramafilm',
+      description: 'gramafilm',
+      display: 'standalone',
+      orientation: 'portrait',
+      background_color: '#000000',
+      msTileColor: '#01485f',
+      theme_color: '#01485f',
+      icons: [
+        {
+          src: '/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/android-chrome-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: '/icons/android-chrome-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+        {
+          src: '/icons/apple-touch-icon-60x60.png',
+          sizes: '60x60',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/apple-touch-icon-76x76.png',
+          sizes: '76x76',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/apple-touch-icon-120x120.png',
+          sizes: '120x120',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/apple-touch-icon-152x152.png',
+          sizes: '152x152',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/apple-touch-icon-180x180.png',
+          sizes: '180x180',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/apple-touch-icon.png',
+          sizes: '180x180',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/favicon-16x16.png',
+          sizes: '16x16',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/favicon-32x32.png',
+          sizes: '32x32',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/msapplication-icon-144x144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: '/icons/mstile-150x150.png',
+          sizes: '150x150',
+          type: 'image/png',
+        }
+      ]
     }
   },
   /*
